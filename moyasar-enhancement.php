9b3afd93ab4845fe49a2d8468ce25983eba6e825
AhmedHany2021 <79626909+AhmedHany2021@users.noreply.github.com>
2024-12-22 10:35:10 +0200

    start plugin

---
+++ moyasar-enhancement.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: moyasar enhancement plugin
+Plugin URI: https://github.com/AhmedHany2021
+Description: this plugin allow moyasar plugin to authorize the payment and the capture it on order status change
+Author: Ahmed Hany
+Version: 1.0.0
+Author URI: https://github.com/AhmedHany2021
+*/
+
+namespace MOYASARENHANCEMENT;
+
+if (!defined('ABSPATH'))
+{
+    die();
+}
+if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    die("NO ACCESS");
+}
+
+/* Add the main global variables */
+
+if(!defined("MOY_BASEDIR")) { define("MOY_BASEDIR",__DIR__ . '/'); }
+if(!defined("MOY_INC")) { define("MOY_INC",MOY_BASEDIR.'includes' . '/'); }
+if(!defined("MOY_TEMPLATES")) { define("MOY_TEMPLATES",MOY_BASEDIR.'templates' . '/'); }
+if(!defined("MOY_URI")) { define("MOY_URI",plugin_dir_url(__FILE__) ); }
+if(!defined("MOY_ASSETS")) { define("MOY_ASSETS", MOY_URI.'assets' . '/'); }
